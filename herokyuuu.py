68bab6f4100a7533d500d8fe0a78206d432972cc
h4ck3r21 <nineplustenequals21.a@gmail.com>
2020-12-29 17:14:13 +1100

    commit init

---
+++ herokyuuu.py
@@ -0,0 +1,37 @@
+from flask import Flask
+
+app = Flask(__name__)
+conversation = ['']
+new_message = ''
+message_num = 0
+
+
+@app.route('/get-message')
+def chat():
+    global new_message
+    message = new_message
+    return message + f'mnum:{message_num}'
+
+
+@app.route('/send-message/<name>/<message>')
+def receive_message(name, message):
+    global message_num
+    global new_message
+    conversation.append(f'[{name}] {message}')
+    message_num += 1
+    new_message = f'[{name}] {message}'
+    return '<html><body><h1>message sent</h1></body></html>'
+
+
+@app.route('/get-previous-messages')
+def send_previous_messages():
+    return '\n'.join(conversation) + f'mnum:{message_num}'
+
+
+@app.route('/')
+def entire_conversation():
+    return str(conversation)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4500, debug=1)

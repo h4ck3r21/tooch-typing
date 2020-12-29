9aff15c81c483e3408e9be825a96eb1ecf15a476
h4ck3r21 <nineplustenequals21.a@gmail.com>
2020-12-29 17:51:42 +1100

    Procfile added

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: flask run
+web: flask run -h 0.0.0.0 -p 80
--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def entire_conversation():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4500, debug=1)
+    app.run()

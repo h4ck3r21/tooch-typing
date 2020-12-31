44d908c811c9b2cd1fbb8e75081fdfd8494a36a4
saman <engsoo.nats@gmail.com>
2020-12-31 11:47:19 +1100

    Finally got working

--- static/sockets.js
+++ static/sockets.js
@@ -2,7 +2,7 @@ $(function() {
     var socket = io.connect(
         'http://' + document.domain + ':' + location.port);
     socket.on( 'connect', function() {
-      socket.emit( 'my event', {data: 'User Connected'})
+      socket.emit( 'my event', {message: 'User Connected'})
       var form = $( 'form' ).on( 'submit', function( e ) {
         e.preventDefault()
         let user_input = $( 'input.message' ).val()
@@ -14,9 +14,11 @@ $(function() {
     });
     socket.on( 'my response', function( msg ) {
         console.log( msg )
-        if( typeof msg.data !== 'undefined' ) {
+        console.log( msg.message )
+        if(msg.data !== 'undefined' ) {
             $( 'h3' ).remove()
-            $( 'div.message_holder' ).append( '<div>'+msg.data+'</div>' )
+            console.log("Appending " + msg.data )
+            $( 'div.message_holder' ).append( '<div>'+msg.message +'</div>' )
         }
     });
 });
